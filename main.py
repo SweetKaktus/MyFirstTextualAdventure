2b27785585419d5ae9a3d3f08a5afae6936e3c38
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2025-05-18 19:54:53 +0200

    Poursuite du process

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import time
 import sys
 
 time_between_lines = 2
-time_between_letters = 0.06
+time_between_letters = 0.05
 
 
-def main_loop():
+def start_game():
 	run : bool = True
-	choix_start = ''
 	while run:
 		title = [
 			'',
@@ -22,41 +21,45 @@ def main_loop():
 			]
 		for element in title:
 			print(element)
-		while choix_start == '':
-			main_menu = [
-					'1. NOUVELLE PARTIE',
-					'2. CHARGER UNE PARTIE',
-					'3. RÈGLES',
-					'4. QUITTER',
-					''
-			]
-			for element in main_menu:
-				print(element)
-			choix = input('Que voulez-vous faire ? (1/2/3/4)\n')
-			print()
-			match choix:
-				case '1':
-					start_scene_1()
-					return
-				case '2':
-					pass
-				case '3':
-					rule_s1 = "Hello ! Moi c'est SweetKaktus ! Bienvenu dans mon premier jeu d'aventure textuel !"
-					rule_s2 = "Tu vas devoir faire des choix afin de progresser dans l'aventure que je te propose !"
-					print_letters_slowly(rule_s1)
-					print_letters_slowly(rule_s2)
-					input("")
-					choix = ''
-				case '4':
-					print_letters_slowly("Merci d'avoir jouer !")
-					print_letters_slowly("À bientôt !")
-					input("")
-					return
-				case _:
-					print_letters_slowly("Votre choix est incorrect, veuillez réessayer.")
-					choix = ''
 
+		launch_main_menu()
 		return
+
+def launch_main_menu():
+	choix = ''
+
+	while choix == '':
+		main_menu = [
+				'1. NOUVELLE PARTIE',
+				'2. CHARGER UNE PARTIE',
+				'3. RÈGLES',
+				'4. QUITTER',
+				''
+		]
+		for element in main_menu:
+			print(element)
+		choix = input('Que voulez-vous faire ? (1/2/3/4)\n')
+		print()
+		match choix:
+			case '1':
+				start_scene_1()
+				return
+			case '2':
+				pass
+			case '3':
+				print_letters_slowly("Hello ! Moi c'est SweetKaktus ! Bienvenu dans mon premier jeu d'aventure textuel !")
+				print_letters_slowly("Tu vas devoir faire des choix afin de progresser dans l'aventure que je te propose !")
+				input("")
+				choix = ''
+			case '4':
+				print_letters_slowly("Merci d'avoir jouer !")
+				print_letters_slowly("À bientôt !")
+				input("")
+				return
+			case _:
+				print_letters_slowly("Votre choix est incorrect, veuillez réessayer.")
+				choix = ''
+	return	
 
 def print_letters_slowly(sentence: str):
 	for character in sentence:
@@ -78,4 +81,4 @@ def start_scene_1():
 	return
 
 if __name__ == '__main__':
-	main_loop()
+	start_game()
